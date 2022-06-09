fa11e3776be46166c0f9d46dcd00cd2b0f09cd59
wilfridbeaunes <kesnelbeaunes@gmail.com>
2022-06-09 12:45:43 +0200

    fix change language

--- SignInForm.Designer.cs
+++ SignInForm.Designer.cs
@@ -43,9 +43,10 @@ namespace pr74_scrum_app
             // 
             this.signinLabel.AutoSize = true;
             this.signinLabel.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
-            this.signinLabel.Location = new System.Drawing.Point(341, 239);
+            this.signinLabel.Location = new System.Drawing.Point(256, 194);
+            this.signinLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.signinLabel.Name = "signinLabel";
-            this.signinLabel.Size = new System.Drawing.Size(58, 17);
+            this.signinLabel.Size = new System.Drawing.Size(45, 13);
             this.signinLabel.TabIndex = 20;
             this.signinLabel.TabStop = true;
             this.signinLabel.Text = "Sign Up";
@@ -54,18 +55,19 @@ namespace pr74_scrum_app
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(173, 239);
+            this.label7.Location = new System.Drawing.Point(130, 194);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(162, 17);
+            this.label7.Size = new System.Drawing.Size(125, 13);
             this.label7.TabIndex = 19;
             this.label7.Text = "Don\'t have an account ?";
             // 
             // SignInButton
             // 
-            this.SignInButton.Location = new System.Drawing.Point(221, 187);
-            this.SignInButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SignInButton.Location = new System.Drawing.Point(166, 152);
+            this.SignInButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SignInButton.Name = "SignInButton";
-            this.SignInButton.Size = new System.Drawing.Size(99, 32);
+            this.SignInButton.Size = new System.Drawing.Size(74, 26);
             this.SignInButton.TabIndex = 18;
             this.SignInButton.Text = "Sign In";
             this.SignInButton.UseVisualStyleBackColor = true;
@@ -75,9 +77,10 @@ namespace pr74_scrum_app
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(55, 138);
+            this.label5.Location = new System.Drawing.Point(41, 112);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(91, 20);
+            this.label5.Size = new System.Drawing.Size(77, 17);
             this.label5.TabIndex = 17;
             this.label5.Text = "Password";
             // 
@@ -85,44 +88,46 @@ namespace pr74_scrum_app
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(95, 94);
+            this.label4.Location = new System.Drawing.Point(71, 76);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.Size = new System.Drawing.Size(47, 17);
             this.label4.TabIndex = 16;
             this.label4.Text = "Email";
             // 
             // signinpasswordText
             // 
-            this.signinpasswordText.Location = new System.Drawing.Point(179, 138);
-            this.signinpasswordText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.signinpasswordText.Location = new System.Drawing.Point(134, 112);
+            this.signinpasswordText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signinpasswordText.Name = "signinpasswordText";
-            this.signinpasswordText.Size = new System.Drawing.Size(265, 22);
+            this.signinpasswordText.Size = new System.Drawing.Size(200, 20);
             this.signinpasswordText.TabIndex = 15;
             this.signinpasswordText.UseSystemPasswordChar = true;
             // 
             // signiemailText
             // 
-            this.signiemailText.Location = new System.Drawing.Point(179, 94);
-            this.signiemailText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.signiemailText.Location = new System.Drawing.Point(134, 76);
+            this.signiemailText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signiemailText.Name = "signiemailText";
-            this.signiemailText.Size = new System.Drawing.Size(265, 22);
+            this.signiemailText.Size = new System.Drawing.Size(200, 20);
             this.signiemailText.TabIndex = 14;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(149, 27);
+            this.label6.Location = new System.Drawing.Point(86, 22);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(250, 29);
+            this.label6.Size = new System.Drawing.Size(233, 24);
             this.label6.TabIndex = 21;
-            this.label6.Text = "Sign In to ScrumApp";
+            this.label6.Text = "Connexion à ScrumApp";
             // 
             // SignInForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(541, 282);
+            this.ClientSize = new System.Drawing.Size(406, 229);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.signinLabel);
             this.Controls.Add(this.label7);
@@ -131,7 +136,7 @@ namespace pr74_scrum_app
             this.Controls.Add(this.label4);
             this.Controls.Add(this.signinpasswordText);
             this.Controls.Add(this.signiemailText);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SignInForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SignIn";
--- SignUpForm.Designer.cs
+++ SignUpForm.Designer.cs
@@ -47,43 +47,43 @@ namespace pr74_scrum_app
             // 
             // firstnameText
             // 
-            this.firstnameText.Location = new System.Drawing.Point(229, 89);
-            this.firstnameText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.firstnameText.Location = new System.Drawing.Point(172, 72);
+            this.firstnameText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.firstnameText.Name = "firstnameText";
-            this.firstnameText.Size = new System.Drawing.Size(265, 22);
+            this.firstnameText.Size = new System.Drawing.Size(200, 20);
             this.firstnameText.TabIndex = 0;
             // 
             // lastnameText
             // 
-            this.lastnameText.Location = new System.Drawing.Point(229, 130);
-            this.lastnameText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lastnameText.Location = new System.Drawing.Point(172, 106);
+            this.lastnameText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lastnameText.Name = "lastnameText";
-            this.lastnameText.Size = new System.Drawing.Size(265, 22);
+            this.lastnameText.Size = new System.Drawing.Size(200, 20);
             this.lastnameText.TabIndex = 1;
             // 
             // emailText
             // 
-            this.emailText.Location = new System.Drawing.Point(229, 174);
-            this.emailText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.emailText.Location = new System.Drawing.Point(172, 141);
+            this.emailText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.emailText.Name = "emailText";
-            this.emailText.Size = new System.Drawing.Size(265, 22);
+            this.emailText.Size = new System.Drawing.Size(200, 20);
             this.emailText.TabIndex = 2;
             // 
             // passwordText
             // 
-            this.passwordText.Location = new System.Drawing.Point(229, 219);
-            this.passwordText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.passwordText.Location = new System.Drawing.Point(172, 178);
+            this.passwordText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.passwordText.Name = "passwordText";
-            this.passwordText.Size = new System.Drawing.Size(265, 22);
+            this.passwordText.Size = new System.Drawing.Size(200, 20);
             this.passwordText.TabIndex = 3;
             this.passwordText.UseSystemPasswordChar = true;
             // 
             // confirmpasswordText
             // 
-            this.confirmpasswordText.Location = new System.Drawing.Point(229, 263);
-            this.confirmpasswordText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.confirmpasswordText.Location = new System.Drawing.Point(172, 214);
+            this.confirmpasswordText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.confirmpasswordText.Name = "confirmpasswordText";
-            this.confirmpasswordText.Size = new System.Drawing.Size(265, 22);
+            this.confirmpasswordText.Size = new System.Drawing.Size(200, 20);
             this.confirmpasswordText.TabIndex = 4;
             this.confirmpasswordText.UseSystemPasswordChar = true;
             // 
@@ -91,29 +91,32 @@ namespace pr74_scrum_app
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(107, 89);
+            this.label1.Location = new System.Drawing.Point(96, 72);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.Size = new System.Drawing.Size(63, 17);
             this.label1.TabIndex = 5;
-            this.label1.Text = "FirstName";
+            this.label1.Text = "Prenom";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(107, 130);
+            this.label2.Location = new System.Drawing.Point(118, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.Size = new System.Drawing.Size(40, 17);
             this.label2.TabIndex = 6;
-            this.label2.Text = "LastName";
+            this.label2.Text = "Nom";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(149, 174);
+            this.label3.Location = new System.Drawing.Point(112, 141);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.Size = new System.Drawing.Size(47, 17);
             this.label3.TabIndex = 7;
             this.label3.Text = "Email";
             // 
@@ -121,9 +124,10 @@ namespace pr74_scrum_app
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(109, 219);
+            this.label4.Location = new System.Drawing.Point(82, 178);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.Size = new System.Drawing.Size(77, 17);
             this.label4.TabIndex = 8;
             this.label4.Text = "Password";
             // 
@@ -131,9 +135,10 @@ namespace pr74_scrum_app
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(29, 263);
+            this.label5.Location = new System.Drawing.Point(22, 214);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(163, 20);
+            this.label5.Size = new System.Drawing.Size(137, 17);
             this.label5.TabIndex = 9;
             this.label5.Text = "Confirm Password";
             // 
@@ -141,18 +146,19 @@ namespace pr74_scrum_app
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(192, 28);
+            this.label6.Location = new System.Drawing.Point(38, 21);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(262, 29);
+            this.label6.Size = new System.Drawing.Size(390, 24);
             this.label6.TabIndex = 10;
-            this.label6.Text = "Sign Up to ScrumApp";
+            this.label6.Text = "Création de votre compte sur  ScrumApp";
             // 
             // SignupButton
             // 
-            this.SignupButton.Location = new System.Drawing.Point(275, 318);
-            this.SignupButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SignupButton.Location = new System.Drawing.Point(206, 258);
+            this.SignupButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SignupButton.Name = "SignupButton";
-            this.SignupButton.Size = new System.Drawing.Size(99, 32);
+            this.SignupButton.Size = new System.Drawing.Size(74, 26);
             this.SignupButton.TabIndex = 11;
             this.SignupButton.Text = "Sign up";
             this.SignupButton.UseVisualStyleBackColor = true;
@@ -161,19 +167,21 @@ namespace pr74_scrum_app
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(227, 370);
+            this.label7.Location = new System.Drawing.Point(170, 301);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(169, 17);
+            this.label7.Size = new System.Drawing.Size(135, 13);
             this.label7.TabIndex = 12;
-            this.label7.Text = "Already has an account ?";
+            this.label7.Text = "Already have an account ?";
             // 
             // signinLabel
             // 
             this.signinLabel.AutoSize = true;
             this.signinLabel.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
-            this.signinLabel.Location = new System.Drawing.Point(403, 370);
+            this.signinLabel.Location = new System.Drawing.Point(302, 301);
+            this.signinLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.signinLabel.Name = "signinLabel";
-            this.signinLabel.Size = new System.Drawing.Size(51, 17);
+            this.signinLabel.Size = new System.Drawing.Size(40, 13);
             this.signinLabel.TabIndex = 13;
             this.signinLabel.TabStop = true;
             this.signinLabel.Text = "Sign In";
@@ -181,9 +189,9 @@ namespace pr74_scrum_app
             // 
             // SignUpForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(621, 423);
+            this.ClientSize = new System.Drawing.Size(466, 344);
             this.Controls.Add(this.signinLabel);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.SignupButton);
@@ -198,7 +206,7 @@ namespace pr74_scrum_app
             this.Controls.Add(this.emailText);
             this.Controls.Add(this.lastnameText);
             this.Controls.Add(this.firstnameText);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SignUpForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SignUp";
