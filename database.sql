b01d90a35bdeba8e5a03de7c82fc863f82794fff
wilfrid beaunes <59376311+wilfridbeaunes@users.noreply.github.com>
2022-06-16 19:18:40 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -2,27 +2,28 @@ create database scrum;
 
 CREATE TABLE `users` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `lastname` varchar(255),
-  `firstname` varchar(255),
+  `name` varchar(255),
+  `prenom` varchar(255),
   `email` varchar(255),
   `password` varchar(255)
 );
 
 CREATE TABLE `Member` (
-  `id` int PRIMARY KEY,
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `Project_id` int,
   `role` varchar(255),
   `user_id` int
 );
 
 CREATE TABLE `Project` (
-  `id` int PRIMARY KEY,
+  `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
   `archived` boolean,
   `created_dt` date
 );
 
 CREATE TABLE `Sprint` (
-  `id` int PRIMARY KEY,
+  `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
   `startDate` date,
   `endDate` date,
@@ -30,7 +31,7 @@ CREATE TABLE `Sprint` (
 );
 
 CREATE TABLE `UserStory` (
-  `id` int PRIMARY KEY,
+  `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
   `description` varchar(255),
   `complexity` int,
@@ -40,7 +41,7 @@ CREATE TABLE `UserStory` (
 );
 
 CREATE TABLE `UserStoryComment` (
-  `id` int PRIMARY KEY,
+  `id` int PRIMARY KEY AUTO_INCREMENT,
   `description` varchar(255),
   `date` datetime,
   `memberId` int,
@@ -55,7 +56,7 @@ CREATE TABLE `UserStoryMember` (
 
 ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `Member` (`user_id`);
 
-ALTER TABLE `Member` ADD FOREIGN KEY (`user_id`) REFERENCES `Project` (`id`);
+ALTER TABLE `Member` ADD FOREIGN KEY (`Project_id`) REFERENCES `Project` (`id`);
 
 ALTER TABLE `Sprint` ADD FOREIGN KEY (`Project_id`) REFERENCES `Project` (`id`);
 
