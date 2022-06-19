35353dd7140128fbbb6aaca583f86a307b1c978b
El Walid Kadura <elwalid.kadura@gmail.com>
2022-06-19 21:39:15 +0200

    feat: adding the begining of sprintView

--- View/SprintForm.Designer.cs
+++ View/SprintForm.Designer.cs
@@ -1,5 +1,8 @@
 ﻿namespace pr74_scrum_app.View
-{
+{   using System.Windows.Forms;
+    using pr74_scrum_app.Controller;
+    using pr74_scrum_app.Model;
+    using System.Collections.Generic;
     partial class SprintForm
     {
         /// <summary>
@@ -28,83 +31,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.inProgressListBox = new System.Windows.Forms.ListBox();
+            this.inReviewListBox = new System.Windows.Forms.ListBox();
+            this.doneListBox = new System.Windows.Forms.ListBox();
+            this.testPanel = new System.Windows.Forms.Panel();
+            this.button1 = new Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.todoListBox = new System.Windows.Forms.Panel();
+            this.testPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // inProgressListBox
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel1.Location = new System.Drawing.Point(602, 77);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(170, 314);
-            this.panel1.TabIndex = 1;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.inProgressListBox.AllowDrop = true;
+            this.inProgressListBox.FormattingEnabled = true;
+            this.inProgressListBox.ItemHeight = 16;
+            this.inProgressListBox.Location = new System.Drawing.Point(530, 188);
+            this.inProgressListBox.Name = "inProgressListBox";
+            this.inProgressListBox.Size = new System.Drawing.Size(248, 500);
+            this.inProgressListBox.TabIndex = 13;
+            this.inProgressListBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox_DragDrop);
+            this.inProgressListBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox_DragEnter);
+            this.inProgressListBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBox_MouseDown);
             // 
-            // label1
+            // inReviewListBox
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(662, 77);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "To Do";
-            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            this.inReviewListBox.AllowDrop = true;
+            this.inReviewListBox.FormattingEnabled = true;
+            this.inReviewListBox.ItemHeight = 16;
+            this.inReviewListBox.Location = new System.Drawing.Point(804, 188);
+            this.inReviewListBox.Name = "inReviewListBox";
+            this.inReviewListBox.Size = new System.Drawing.Size(248, 500);
+            this.inReviewListBox.TabIndex = 14;
+            this.inReviewListBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox_DragDrop);
+            this.inReviewListBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox_DragEnter);
+            this.inReviewListBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBox_MouseDown);
             // 
-            // panel2
+            // doneListBox
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel2.Location = new System.Drawing.Point(402, 77);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(170, 314);
-            this.panel2.TabIndex = 3;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.doneListBox.AllowDrop = true;
+            this.doneListBox.FormattingEnabled = true;
+            this.doneListBox.ItemHeight = 16;
+            this.doneListBox.Location = new System.Drawing.Point(1076, 188);
+            this.doneListBox.Name = "doneListBox";
+            this.doneListBox.Size = new System.Drawing.Size(248, 500);
+            this.doneListBox.TabIndex = 15;
+            this.doneListBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox_DragDrop);
+            this.doneListBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox_DragEnter);
+            this.doneListBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBox_MouseDown);
             // 
-            // label3
+            // testPanel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(262, 77);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 16);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "To Do";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.testPanel.Controls.Add(this.button1);
+            this.testPanel.Controls.Add(this.button2);
+            this.testPanel.Controls.Add(this.button3);
+            this.testPanel.Location = new System.Drawing.Point(46, 188);
+            this.testPanel.Name = "testPanel";
+            this.testPanel.Size = new System.Drawing.Size(200, 500);
+            this.testPanel.TabIndex = 16;
             // 
-            // panel3
+            // button1
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel3.Location = new System.Drawing.Point(202, 77);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(170, 314);
-            this.panel3.TabIndex = 5;
-            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "WEEEESH 1 ! ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 100);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "WEEEESH 2 ! ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 100);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "WEEEESH 3 ! ";
+            // 
+            // todoListBox
+            // 
+            this.todoListBox.AllowDrop = true;
+            this.todoListBox.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.todoListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.todoListBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.todoListBox.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+            this.todoListBox.Location = new System.Drawing.Point(259, 188);
+            this.todoListBox.Margin = new System.Windows.Forms.Padding(25);
+            this.todoListBox.Name = "todoListBox";
+            this.todoListBox.Size = new System.Drawing.Size(248, 498);
+            this.todoListBox.TabIndex = 4;
+            this.todoListBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox_DragDrop);
+            this.todoListBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox_DragEnter);
+            this.todoListBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBox_MouseDown);
             // 
             // SprintForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(974, 536);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.testPanel);
+            this.Controls.Add(this.todoListBox);
+            this.Controls.Add(this.doneListBox);
+            this.Controls.Add(this.inReviewListBox);
+            this.Controls.Add(this.inProgressListBox);
             this.Name = "SprintForm";
             this.Text = "Sprint View";
-            this.Load += new System.EventHandler(this.SprintForm_Load);
+            this.testPanel.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
-
         #endregion
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListBox inProgressListBox;
+        private System.Windows.Forms.ListBox inReviewListBox;
+        private System.Windows.Forms.ListBox doneListBox;
+        private System.Windows.Forms.Panel testPanel;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        public Panel todoListBox;
     }
 }
--- View/SprintForm.cs
+++ View/SprintForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using pr74_scrum_app.Controller;
+using pr74_scrum_app.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,63 +10,54 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace pr74_scrum_app.View
 {
     public partial class SprintForm : Form
     {
-        public SprintForm()
+        int index = 0;
+        Sprint sprint;
+        ListBox source;
+        public SprintForm(int id)
         {
             InitializeComponent();
+            SprintController sprintController = new SprintController();
+            sprint = sprintController.FetchSprintById(id);
+            if(sprint == null)
+                // TODO: redirection vers menu project
+                MessageBox.Show("404 : Sprint not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
-        private void SprintForm_Load(object sender, EventArgs e)
+        private void ListBox_MouseDown(object sender, MouseEventArgs e)
         {
-
+            source = sender as ListBox;
+            index = source.IndexFromPoint(e.X, e.Y);
+            if (index >= 0)
+            {
+                source.DoDragDrop(source.Items[index].ToString(), DragDropEffects.Move);
+            }
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void ListBox_DragDrop(object sender, DragEventArgs e)
         {
-
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        private void ListBox_DragEnter(object sender, DragEventArgs e)
         {
-
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
+            ListBox actuel = sender as ListBox;
+            if(actuel != source)
+            {
+                actuel.Items.Add(source.Items[index]);
+                source.Items.RemoveAt(index);
+                source = actuel;
+                index = source.Items.Count-1;
+            }
         }
     }
 }
