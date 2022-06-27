008c16e990ca9d46f8e239d1202c4e7d013f2d63
malo <malo.lm@icloud.com>
2022-06-27 21:52:06 +0200

    sql request ONLY

--- Controller/SidebarController.cs
+++ Controller/SidebarController.cs
@@ -20,7 +20,7 @@ namespace pr74_scrum_app
         public Dictionary<int,string> GetProjects(int userID, bool pinned)
         {
             Dictionary<int, string> projects = new Dictionary<int, string>();
-            string sql = $"select * from Member m inner join Project p on m.Project_id = p.id where m.pinned = {pinned} and user_id='{userID}' order by p.id DESC LIMIT 5";
+            string sql = $"select * from Member m inner join Project p on m.Project_id = p.id where m.pinned = {pinned} and user_id='{userID}' and archived = 0 order by p.id DESC LIMIT 5";
             MySqlDataReader dr = db.ExecutQuery(sql);
             if (dr.HasRows)  while (dr.Read()) projects.Add((int)dr["Project_id"], (string)dr["name"]);
             dr.Close();
